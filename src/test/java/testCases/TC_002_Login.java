4ebf69a129947c21722cc160f68b08798a0b81c6
pavanoltraining <pavanoltraining@gmail.com>
2021-10-25 07:42:12 +0530

    files are modified

--- src/test/java/testCases/TC_002_Login.java
+++ src/test/java/testCases/TC_002_Login.java
@@ -7,6 +7,8 @@ import pageObjects.HomePage;
 import pageObjects.LoginPage;
 import testBase.BaseClass;
 //This is Login Test
+// This file is modified.
+
 public class TC_002_Login extends BaseClass
 {
 	@Test(groups= {"sanity","master"})
